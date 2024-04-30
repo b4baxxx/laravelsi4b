80bb35ee4d294aea2a205dfb0b660d6d5cdf9fc5
Babaxx <160828060+b4baxxx@users.noreply.github.com>
2024-04-30 16:54:21 +0700

    instal laravel

---
+++ laravelsi4b/example-app/resources/views/profil.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta >
+    <title>profil</title>
+</head>
+<body>
+    <h1>profil</h1>
+    <p>Ini halaman profil</p>
+</body>
+</html>

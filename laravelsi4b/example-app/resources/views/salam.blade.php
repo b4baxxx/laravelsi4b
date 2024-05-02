c881b0773dfa0020a7c8188f8a2f3e4241b4137b
Marcelino2226250001 <160828060+b4baxxx@users.noreply.github.com>
2024-05-02 15:15:36 +0700

    new

---
+++ laravelsi4b/example-app/resources/views/salam.blade.php
@@ -0,0 +1,6 @@
+<h1>Program Studi</h1>
+<ul>
+    @foreach($viewlist as $item)
+    <li>{{ $item }}</li>
+    @endforeach
+</ul>
--- laravelsi4b/example-app/routes/web.php
+++ laravelsi4b/example-app/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\FakultasController;
+use App\Models\Fakultas;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +17,23 @@ Route::get('profil', function(){
     return view('profil');
 
 });
+    // route dengan parameter
+    Route::get('welcome/{salam}', function ($salam){
+        // return'selamat '. $salam;
+        return view('salam')->with('viewsalam', $salam);
+    });
+
+// route tanpa parameter
+// terdapat array $list
+Route::get('listdata', function () {
+    $list = ["Sistem Informasi", "Informatika", "Manajemen"];
+    $listmhs = [
+        ["npm" => 001, "nama" => "ahmad" ],
+        ["npm" => 002, "nama" => "ratno" ]
+    ];
+    return view('listprodi') 
+    ->with('viewlist', $list)
+    ->with('viewmhs', $listmhs);
+});
+
+Route::resource('fakultas', FakultasController::class);

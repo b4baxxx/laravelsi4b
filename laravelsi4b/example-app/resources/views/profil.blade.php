7e445119e361b0500ff41e35d6cf4990bd33b7c5
b4baxxx <zidanyyu@gmail.com>
2024-06-04 15:33:31 +0700

    p

--- laravelsi4b/example-app/resources/views/profil.blade.php
+++ laravelsi4b/example-app/resources/views/profil.blade.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta >
-    <title>profil</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profil</title>
 </head>
+
 <body>
-    <h1>profil</h1>
-    <p>Ini halaman profil</p>
+    <h1>Profil</h1>
+    <p>ini halaman profil</p>
 </body>
 </html>
